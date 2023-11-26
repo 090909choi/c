e9a1e521613a6bddd6d1eb5edbb893083bf76082
090909choi <144232279+090909choi@users.noreply.github.com>
2023-11-26 10:12:33 +0900

    11/26
    11/26

--- 이름없음12.cpp
+++ 이름없음12.cpp
@@ -3,13 +3,15 @@
 int ReverseNum(int n)
 {
 	int sum1,sum2;
+	int a = 1;
 	
 	sum2=0;
-	for(int i = 10; i<=n; i*=10)
+	for(int i = 10; i<=n*10; i*=10)
 	{
 		sum1=n%i-sum2;
-		printf("%d",sum1/(i/10));
+		printf("%d",sum1/a);
 		sum2=sum1+sum2;
+		a*=10;
 	}
 	return sum1;
 }
@@ -17,5 +19,5 @@ int ReverseNum(int n)
 int main()
 {
 	
-	int re = ReverseNum(123);
+	int re = ReverseNum(345);
 }
