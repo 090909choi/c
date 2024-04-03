c08b448767be68163741b7b149139fe4011df975
090909choi <144232279+090909choi@users.noreply.github.com>
2024-04-03 19:51:36 +0900

    4/3
    4/3

--- 이름없음1.cpp
+++ 이름없음1.cpp
@@ -2,31 +2,19 @@
 
 int main()
 {
-	int n,m,sum = 0,sum1 = 0;
-	scanf("%d %d",&n,&m);
-	int ar[m][n] = {0,};
-	
-	for(int i = 0;i<m;i++){
-		for(int j = 0;j<n;j++){
-			scanf("%d",&ar[i][j]);
+	int n,t,k,ar[100][100],tx,ty,kx,ky;
+	scanf("%d",&n);
+	scanf("%d %d",&t,&k);
+	for(int i = 0;i<t;i++){
+		scanf("%d %d",&tx,&ty);
+		ar[ty][tx] = -1;
+	}
+	for(int i = 0;i<k;i++){
+		scanf("%d %d",&kx,&ky);
+		if(ar[ky][kx] == -1)
+		{
+			printf("%d",i+1);
 		}
 	}
-	
-	for(int i = 0;i<m;i++){
-		sum = 0;
-		for(int j = 0;j<n;j++){
-			sum += ar[i][j];
-			sum += ar[i-1][j];
-			sum += ar[i+1][j];
-			sum += ar[i][j-1];
-			sum += ar[i][j+1];
-			if(sum1 < sum)
-			{
-				sum1 = sum;
-			}
-			sum = 0;
-		}
-	}
-	printf("%d",sum1);
 	return 0;
 }
