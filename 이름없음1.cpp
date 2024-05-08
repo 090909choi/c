1ea94c41c4771f94612ed58608f6f215c37f60c3
090909choi <144232279+090909choi@users.noreply.github.com>
2024-05-08 18:45:17 +0900

    5/8
    5/8

--- 이름없음1.cpp
+++ 이름없음1.cpp
@@ -1,11 +1,32 @@
 #include<stdio.h>
+#include<string.h>
 
 int main()
 {
-	char a[10001];
-	char b[10001];
-	int n;
-	
-	scanf("%d",&n);
-	
+	char a[10000],b[10000];
+	scanf("%s %s",a,b);
+	if(strlen(a) > strlen(b))
+	{
+		printf("sir");
+	}
+	else if(strlen(a) < strlen(b))
+	{
+		printf("kid");
+	}
+	else if(strlen(a) == strlen(b))
+	{
+		for(int i = 0;i<strlen(a);i++){
+			if(a[i] < b[i])
+			{
+				printf("kid");
+				break;
+			}
+			else if(a[i] > b[i])
+			{
+				printf("sir");
+				break;
+			}
+		}
+	}
+	return 0;
 }
--- 이름없음2.cpp
+++ 이름없음2.cpp
@@ -1,14 +1,42 @@
 #include<stdio.h>
-#include<string.h> 
 
 int main()
 {
-	char a[1000];
-	char b[1000];
-	int n;
-	scanf("%s",a);
-	scanf("%s",b);
-	n = strcmp(a,b);
-	printf("%d",n);
+	int n,m;
+	char c;
+	scanf("%d",&n);
+	for(int i = 0;i<n;i++)
+	{
+		scanf("%d %c",&m,&c);
+		if(i == n-1)
+		{
+			scanf("%d",&n);
+			if('a' <= c && c <= 'z')
+			{
+				if(m -c+'a'-1 < 1)
+				{
+					printf("1");
+				}
+				else
+				{
+					printf("%d",m -c+'a'-1);
+				}
+				break;
+			}
+			else
+			{
+				if(m + c-'A'+1 > 50)
+				{
+					printf("50");
+				}
+				else
+				{
+					printf("%d",m + c-'A'+1);
+				}
+				break;
+			}
+		}
+		
+	}
 	return 0;
 }
