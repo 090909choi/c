6745d828c72086a119a4cc979fe98301aa6bc5ed
090909choi <144232279+090909choi@users.noreply.github.com>
2024-05-22 20:13:36 +0900

    5/22
    5/22

--- 이름없음1.cpp
+++ 이름없음1.cpp
@@ -1,24 +1,35 @@
 #include<stdio.h>
-#include<queue>
 #include<stack>
+#include<queue>
 #include<iostream>
 using namespace std;
+
 int main()
 {
-	stack<int>s;
-	queue<int> q;
-	for(int i = 1;i<11;i++){
-		q.push(i);
-		s.push(i);
+	stack<int> a;
+	char n[101];
+	cin >> n;
+	for(int i = 0;n[i];i++){
+		if('0' <= n[i] && n[i] <= '9')
+		{
+			a.push(n[i] - '0');
+		}
+		else
+		{
+			int b = a.top();
+			a.pop();
+			int c = a.top();
+			a.pop();
+			if(n[i] == '+')
+			{
+				a.push(c+b);
+			}
+			else
+			{
+				a.push(c-b);
+			}
+		}
 	}
-	while(!q.empty()){
-		cout << q.front() << " ";
-		q.pop();
-	}
-	cout << "\n";
-	while(!s.empty()){
-		cout <<s.top()<<" ";
-		s.pop();
-	}
+	cout << a.top();
 	return 0;
 }
--- 이름없음2.cpp
+++ 이름없음2.cpp
@@ -1,26 +1,34 @@
 #include<stdio.h>
-#include<queue>
 #include<stack>
+#include<queue>
 #include<iostream>
 using namespace std;
+
 int main()
 {
-	stack<int> m;
-	queue<string>a;
-	int q,n;
-	string w;
-	cin >> n;
-	for(int i = 0;i<n;i++){
-		cin >> q >> w;
-		m.push(q);
-		a.push(w);
+	int a,b;
+	cin >> a >> b;
+	queue<pair<int,int>> q;
+	q.push(make_pair(a,0));
+	while(!q.empty())
+	{
+		int num = q.front().first;
+		int count = q.front().second;
+		q.pop();
+		if(num == b)
+		{
+			cout >> count;
+			return 0;
+		}
+		if(num * 2 <= b)
+		{
+			q.push(make_pai(num*2,count+1));
+		}
+		if(num*10+1<=b)
+		{
+			q.push(make_pair(num*10+1,count+1));
+		}
 	}
-	while(!m.empty()){
-		cout << m.top() << " ";
-		m.pop();
-	}
-	while(!a.empty()){
-		cout << a.front() << " ";
-		a.pop();
-	}
+	cout >> "-1";
+	return 0;
 }
