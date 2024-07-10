c2626620d2994a8f4f51cded741156a5b105bbf5
090909choi <144232279+090909choi@users.noreply.github.com>
2024-07-10 19:44:56 +0900

    7/10
    7/10

--- 이름없음2.cpp
+++ 이름없음2.cpp
@@ -1,29 +1,32 @@
 #include<stdio.h>
-int d[5001] = {0,};
 
-int dp(int x){
-	
-}
+int d[1000001];
 
-int main(){
+int main() 
+{
 	int n;
+	scanf("%d", &n);
+	d[1] = 0;
+	d[2] = 1;
 	d[3] = 1;
-	d[5] = 1;
-	scanf("%d",&n);
-	
-	for(int i = 6;i<=n;i++){
-		if(d[i-3]){
-			d[i] = d[i-3] + 1;
+	for(int i = 4; i <= n; i++) 
+	{
+		d[i] = d[i - 1] + 1;
+
+		if (i % 2 == 0){
+			if (d[i] > d[i / 2] + 1){
+				d[i] = d[i / 2] + 1;
+				} 
+			}
+
+		if (i % 3 == 0){
+			if (d[i] > d[i / 3] + 1){
+				d[i] = d[i / 3] + 1; 
+			} 
 		}
-		if(d[i-5]){
-			d[i] = d[i-5] + 1;
-		}
-	}	
 	
-	if(d[n]){
-		printf("%d",d[n]);
 	}
-	else{
-		printf("-1");
-	}
+
+	printf("%d", d[n]);
+	return 0;
 }
