1823ac2b4f985a5dc474c35a7ad0c22711b7a561
090909choi <144232279+090909choi@users.noreply.github.com>
2024-07-10 19:41:58 +0900

    7/10
    7/10

--- 이름없음1.cpp
+++ 이름없음1.cpp
@@ -1,22 +1,23 @@
 #include<stdio.h>
 
-int fibo(int x){
-	
-	if(x==1){
-		return 1;
-	}
-	if(x==2){
-		return 1;
-	}
-	if(d[x] != 0){
-		return d[x];
-	}
-	return d[x] = fibo(x-1) + fibo(x-2);
-}
-
-int main(){
-	int n;
+int main()
+{
+	int n,sum = 0;
 	scanf("%d",&n);
-	printf("%d",fibo(n));
-	return 0;
+	while(n>=1){
+		if(n%2==0 && n%3==0){
+			sum++;
+			n+=1;
+		}
+		
+		if(n%3 == 0){
+			sum++;
+			n/=3;
+		}
+		if(n%2==0){
+			sum++;
+			n/=2;
+		}
+	}
+	printf("%d",sum);
 }
